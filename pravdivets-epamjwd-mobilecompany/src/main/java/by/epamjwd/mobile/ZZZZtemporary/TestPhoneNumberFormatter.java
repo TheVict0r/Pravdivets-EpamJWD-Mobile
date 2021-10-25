9bb9d1057a41140d94185cdf10c13b3280bfaf79
vpravd1@gmail.com <Vv77100369>
2021-10-25 11:20:48 +0300

    1

--- pravdivets-epamjwd-mobilecompany/src/main/java/by/epamjwd/mobile/ZZZZtemporary/TestPhoneNumberFormatter.java
+++ pravdivets-epamjwd-mobilecompany/src/main/java/by/epamjwd/mobile/ZZZZtemporary/TestPhoneNumberFormatter.java
@@ -6,20 +6,18 @@ import by.epamjwd.mobile.util.PhoneFormatter;
 public class TestPhoneNumberFormatter {
 public static void main(String[] args) {
 	
-	String goodPhoneNumber25 = "258901235";
-	String goodPhoneNumber29 = "295678901";
-	String goodPhoneNumber33 = "332345678";
-	String goodPhoneNumber44 = "449012345";
-	String goodPhoneNumber55 = "1290123235";
+	int goodPhoneNumber25 = 258901235;
+	int goodPhoneNumber29 = 295678901;
+	int goodPhoneNumber33 = 332345678;
+	int goodPhoneNumber44 = 449012345;
+	int badPhoneNumber55 = 1290123235;
 
-	
-	PhoneFormatter formatter = new PhoneFormatter();
 	try {
-	System.out.println(formatter.formatPhone(goodPhoneNumber25));
-	System.out.println(formatter.formatPhone(goodPhoneNumber29));
-	System.out.println(formatter.formatPhone(goodPhoneNumber33));
-	System.out.println(formatter.formatPhone(goodPhoneNumber44));
-	System.out.println(formatter.formatPhone(goodPhoneNumber55));
+		System.out.println(PhoneFormatter.formatPhone(goodPhoneNumber25));
+		System.out.println(PhoneFormatter.formatPhone(goodPhoneNumber29));
+		System.out.println(PhoneFormatter.formatPhone(goodPhoneNumber33));
+		System.out.println(PhoneFormatter.formatPhone(goodPhoneNumber44));
+		System.out.println(PhoneFormatter.formatPhone(badPhoneNumber55));
 	} catch (ServiceException e) {
 		System.out.println("Но шо-та пошло не так!");
 	}
--- pravdivets-epamjwd-mobilecompany/src/test/java/by/epamjwd/mobile/util/PhoneNumberFormatterTest.java
+++ pravdivets-epamjwd-mobilecompany/src/test/java/by/epamjwd/mobile/util/PhoneNumberFormatterTest.java
@@ -1,33 +1,25 @@
 package by.epamjwd.mobile.util;
 
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import by.epamjwd.mobile.service.exception.ServiceException;
 
 public class PhoneNumberFormatterTest {
 
-	private static PhoneFormatter formatter;
-	
-	private final String goodNumber1 = "258901235";
-	private final String goodNumber2 = "295678901";
-	private final String goodNumber3 = "332345678";
-	private final String goodNumber4 = "449012345";
+	private final int goodNumber1 = 258901235;
+	private final int goodNumber2 = 295678901;
+	private final int goodNumber3 = 332345678;
+	private final int goodNumber4 = 449012345;
 	
 	private final String goodNumber1Result = "(25) 890-12-35";
 	private final String goodNumber2Result = "(29) 567-89-01";
 	private final String goodNumber3Result = "(33) 234-56-78";
 	private final String goodNumber4Result = "(44) 901-23-45";
 	
-	private final String badNumber1  = "22123235";
-	private final String badNumber2  = "1290123235";
-
-	@BeforeClass
-	public static void createPhoneNumberFormatter() {
-		formatter = new PhoneFormatter();
-	}
-
+	private final int badNumber1  = 22123235;
+	private final int badNumber2  = 1290123235;
+	
 	@Test
 	public void testGoodNumber1() throws ServiceException {
 		Assert.assertEquals(goodNumber1Result, PhoneFormatter.formatPhone(goodNumber1));
