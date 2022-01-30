05e64b2328a19c5d28f94df2b2e5c2c1d38e37d2
vpravd1@gmail.com <Vv77100369>
2022-01-30 15:31:38 +0300

    JavaDoc added

--- pravdivets-epamjwd-mobilecompany/src/main/java/by/epamjwd/mobile/controller/command/impl/LoginCommand.java
+++ pravdivets-epamjwd-mobilecompany/src/main/java/by/epamjwd/mobile/controller/command/impl/LoginCommand.java
@@ -1,6 +1,5 @@
 package by.epamjwd.mobile.controller.command.impl;
 
-import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,11 +9,13 @@ import javax.servlet.http.HttpSession;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import by.epamjwd.mobile.bean.Role;
 import by.epamjwd.mobile.bean.Subscriber;
 import by.epamjwd.mobile.bean.User;
 import by.epamjwd.mobile.controller.RouteHelper;
 import by.epamjwd.mobile.controller.RouteMethod;
 import by.epamjwd.mobile.controller.command.Command;
+import by.epamjwd.mobile.controller.command.UserRolePathProvider;
 import by.epamjwd.mobile.controller.command.helpers.SubscriberCommandHelper;
 import by.epamjwd.mobile.controller.repository.AttributeName;
 import by.epamjwd.mobile.controller.repository.AttributeValue;
@@ -30,6 +31,17 @@ public class LoginCommand implements Command {
 
 	private final static Logger LOGGER = LogManager.getLogger(LoginCommand.class);
 
+	/**
+	 * The login could be a phone number for Subscribers and an e-mail for all kind of Users
+	 * (including Subscribers). 
+	 * 
+	 * If Subscriber has multiple phone numbers and use an e-mail as a login, 
+	 * the menu with all his/her numbers will appear. 
+	 * 
+	 * If Subscriber use phone number as a login 
+	 * the detailed information about this particular phone number only will appear
+	 * (despite the amount of phone numbers this Subscriber has).
+	 */
 	@Override
 	public RouteHelper execute(HttpServletRequest request, HttpServletResponse response) {
 		UserService userService = ServiceProvider.getInstance().getUserService();
@@ -37,11 +49,11 @@ public class LoginCommand implements Command {
 		HttpSession session = request.getSession();
 		RouteHelper result = RouteHelper.ERROR;
 		User user = null;
+		Role role = null;
 
 		String login = request.getParameter(ParameterName.LOGIN);
 
-		if (    login == null || login.isBlank() 
-				|| request.getParameter(ParameterName.PASSWORD) == null
+		if (login == null || login.isBlank() || request.getParameter(ParameterName.PASSWORD) == null
 				|| request.getParameter(ParameterName.PASSWORD).isBlank()) {
 			session.setAttribute(AttributeName.ERROR, AttributeValue.ERROR_LOGIN);
 			return new RouteHelper(PagePath.LOGIN_REDIRECT, RouteMethod.REDIRECT);
@@ -50,8 +62,7 @@ public class LoginCommand implements Command {
 		try {
 			Optional<User> userOptional = userService.findUserByLogin(login);
 
-			if (userOptional.isPresent() 
-					&& userService.isPasswordCorrect(userOptional.get(),
+			if (userOptional.isPresent() && userService.isPasswordCorrect(userOptional.get(),
 					request.getParameter(ParameterName.PASSWORD))) {
 				user = userOptional.get();
 				session.setAttribute(AttributeName.USER_ID, user.getId());
@@ -61,42 +72,31 @@ public class LoginCommand implements Command {
 			} else {
 				session.setAttribute(AttributeName.ERROR, AttributeValue.ERROR_LOGIN);
 				session.setAttribute(AttributeName.LOGIN, login);
-				session.setAttribute(AttributeName.PASSWORD, 
-						request.getParameter(ParameterName.PASSWORD));
+				session.setAttribute(AttributeName.PASSWORD, request.getParameter(ParameterName.PASSWORD));
 				return new RouteHelper(PagePath.LOGIN_REDIRECT, RouteMethod.REDIRECT);
 			}
 		} catch (ServiceException e) {
 			LOGGER.error("Unable to retrieve user data for login - " + login, e);
 			return RouteHelper.ERROR_500;
 		}
-		
-		switch (user.getRole()) {
-		case ADMIN:
-			result = new RouteHelper(PagePath.ADMIN_REDIRECT, RouteMethod.REDIRECT);
-			break;
-		case CONSULTANT:
-			result = new RouteHelper(PagePath.SUBSCRIBER_OPERATIONS_REDIRECT, RouteMethod.REDIRECT);
-			break;
-		case SUBSCRIBER:
-			try {
-				if (InputDataValidator.isPhone(login) && subscriberService.isPhoneExist(login)) {
-					Subscriber subscriber = subscriberService.findSubscriberByPhone(login).get();
-					result = SubscriberCommandHelper.getInstance().handleSubscriber(request, subscriber);
-				} else {// this means login is an e-mail and one User could have multiple phone numbers 
-						//(and multiple Subscriber entities as well)
-					List<Subscriber> subscriberList = subscriberService.findSubscriberListByUserId(user.getId());
-					result = SubscriberCommandHelper.getInstance().handleSubscriberListRedirect(request,
-							subscriberList);
-				}
-			} catch (ServiceException e) {
-				LOGGER.error("Unable to retrieve Subscriber data for user ID - " + user.getId(), e);
-				result = RouteHelper.ERROR_500;
+
+		role = user.getRole();
+		result = UserRolePathProvider.getInstance().providePath(role);
+
+		try {
+			if (InputDataValidator.isPhone(login) && subscriberService.isPhoneExist(login)) {
+				Subscriber subscriber = subscriberService.findSubscriberByPhone(login).get();
+				/* we can safely use .get() method as just checked that this 
+				 * Optional<Subscriber> is not empty in the previous line - 
+				 * see subscriberService.isPhoneExist(login)
+				 */
+				result = SubscriberCommandHelper.getInstance().handleSubscriber(request, subscriber);
 			}
-			break;
-		default:
-			result = new RouteHelper(PagePath.LOGIN_REDIRECT, RouteMethod.REDIRECT);
+		} catch (ServiceException e) {
+			LOGGER.error("Unable to retrieve Subscriber data for phone - " + login, e);
+			result = RouteHelper.ERROR_500;
 		}
-		
+
 		return result;
 	}
 
